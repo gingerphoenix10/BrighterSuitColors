17a63dc03db6c8d4fd384732b53fa2536623c760
gingerphoenix10 <gingerphoenix10@gmail.com>
2024-12-22 16:14:21 +0000

    Likely the only commit

---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BrigherSuitColors;
+
+[ContentWarningPlugin("Brighter Suit Colors", "1.0.0", true)]
+
+[HarmonyPatch(typeof(PlayerVisor))]
+public class PlayerVisorPatch
+{
+    [HarmonyPatch("Update")]
+    [HarmonyPrefix]
+    private static void UpdatePatch(PlayerVisor __instance)
+    {
+        if (__instance == null || __instance.visorColor == null)
+        {
+            return;
+        }
+
+        Color color = __instance.visorColor.Value;
+        SkinnedMeshRenderer[] componentsInChildren = __instance.transform.GetChild(1).GetComponentsInChildren<SkinnedMeshRenderer>();
+        for (int i = 0; i < componentsInChildren.Length; i++)
+        {
+            componentsInChildren[i].material.color = color;
+        }
+    }
+}
